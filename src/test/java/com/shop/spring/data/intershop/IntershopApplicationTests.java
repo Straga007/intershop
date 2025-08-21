3158c313ad3520fd9b9066732fbc8e155b7bb91f
Straga007 <Straga07@yandex.ru>
2025-08-21 17:12:03 +0300

    Initial commit

---
+++ src/test/java/com/shop/spring/data/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.spring.data.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
