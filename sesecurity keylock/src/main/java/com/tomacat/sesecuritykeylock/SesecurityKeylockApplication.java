12e5814ff22cb8af58ea5baa8ed29e7b3c4b5028
Straga007 <Straga07@yandex.ru>
2025-11-09 14:28:02 +0300

    this shod be on the user branch

---
+++ sesecurity keylock/src/main/java/com/tomacat/sesecuritykeylock/SesecurityKeylockApplication.java
@@ -0,0 +1,14 @@
+package com.tomacat.sesecuritykeylock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesecurityKeylockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesecurityKeylockApplication.class, args);
+	}
+
+}
+
---
+++ sesecurity keylock/src/test/java/com/tomacat/sesecuritykeylock/SesecurityKeylockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomacat.sesecuritykeylock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesecurityKeylockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
